73c5825baef58c1c33b19af9b9268b78ec601caa
Gabriel-KQ <Gabriel.Wairimu@kenya-airways.com>
2025-08-08 11:56:27 +0300

    git practice

---
+++ WebApplication-learning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication_learning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
